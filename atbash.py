f87a00d9bc499e000a8b3513d3848d1c619d701a
BenKeeping03 <benkeeping@hotmail.co.uk>
2025-08-29 13:34:37 +0100

    Completed Atbash Decryption

--- atbash.py
+++ atbash.py
@@ -1,14 +1,17 @@
 input_phrase = ""
 encrypted_phrase = ""
+decrypted_phrase = ""
 char_ascii = 0
 def choice():
     option = int(input("Choose 1 for Encrypt, 2 for Decrypt: "))
     if option == 1:
         atbash_encrypt()
+    if option  == 2:
+        atbash_decrypt()
 
 def atbash_encrypt():
     global encrypted_phrase
-    phrase = str(input("Type phrase to input: "))
+    phrase = str(input("Type phrase to Encrypt: "))
     for char in phrase:
         char_ascii = ord(char)
         if 65 <= char_ascii <= 90:
@@ -26,6 +29,23 @@ def atbash_encrypt():
     print(encrypted_phrase)
         
 
+def atbash_decrypt():
+    global decrypted_phrase
+    phrase = str(input("Type phrase to Decrypt: "))
+    for char in phrase:
+        char_ascii = ord(char)
+        if 65 <= char_ascii <= 90:
+            difference = 90 - char_ascii 
+            new_letter = chr(65 + difference) 
 
+        elif 97 <= char_ascii <= 122:
+            difference = 122 - char_ascii
+            new_letter = chr(97 + difference)
+            
+        else:
+            new_letter = char
+
+        decrypted_phrase += new_letter
+    print(decrypted_phrase)
 
 choice()
