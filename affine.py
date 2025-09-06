b6df2ed9a62f7c4b43916dd644f83bdda4e82734
BenKeeping03 <benkeeping@hotmail.co.uk>
2025-09-06 18:46:18 +0100

    Modular inverse of A added for decryption + decryption is working as required

--- affine.py
+++ affine.py
@@ -1,5 +1,6 @@
 char_ascii = 0
 encrypted_phrase = ""
+decrypted_phrase = ""
 def choice():
     option = int(input("Enter 1 to Encrypt, 2 to Decrypt: "))
     if option == 1:
@@ -7,6 +8,7 @@ def choice():
         affine_encrypt()
     elif option == 2:
         print("op2")
+        affine_decrypt()
     else:
         print("Select a valid input (1 or 2)")
         choice()
@@ -27,4 +29,22 @@ def affine_encrypt():
             encrypted_phrase += char
         
     print(encrypted_phrase)
+
+def affine_decrypt():
+    phrase = str(input("Type a phrase to decrypt: "))
+    a = int(input("Type the value for A: "))
+    b = int(input("Type the value for B: "))
+    global decrypted_phrase
+    for char in phrase:
+        if ((65 <= ord(char)<=90) or (97 <= ord(char) <= 122)):
+            x = ord(char.lower()) - 97
+            a_inverse = pow(a, -1, 26)
+            y = (a_inverse * (x - b))%26
+            new_letter = chr(y + 97)
+            print(y)
+            decrypted_phrase += new_letter
+        else:
+            decrypted_phrase += char
+    print(decrypted_phrase)
+        
 choice()
