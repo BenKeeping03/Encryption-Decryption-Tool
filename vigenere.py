83d41fce497123dae9baad0c736654cef366c43c
BenKeeping03 <benkeeping@hotmail.co.uk>
2025-09-10 22:01:52 +0100

    Encryption now working for vigenere, symbols need finishing

--- vigenere.py
+++ vigenere.py
@@ -1,6 +1,9 @@
+
 def vig_encrypt():
     phrase = str(input("Enter a phrase to encrypt: "))
+    phrase = phrase.upper()
     key = str(input("Enter a key to encrypt with: "))
+    key = key.upper()
     extra = 0
     keyword = ""
     keyword = keyword + key
@@ -14,23 +17,47 @@ def vig_encrypt():
     print(len(keyword))
     print(keyword)
 
-    rows = 26
-    col = 26
+    
     vig_matrix = []
     
     i=0
     y=0
     start = 0
+    
     for i in range(26):
+        line = []
         for y in range(26):
+            
             if (65 <= 65 + y + start <= 90):
-                print(chr(65 + y + start), end="")
+                line.append(chr(65 + y + start)) 
             else:
                 letter = chr((y + start) % 26 + 65)
-                print(letter , end="")
-        print()
+                line.append(letter)
+            
+        vig_matrix.append(line)
+        line = ""
+    
       
         start += 1
+    print(vig_matrix)
+
+
+    number = 0
+    encrypted_phrase = ""
+    for x in keyword:
+        
+        row_num = (ord(keyword[number])-65)
+        col_num = (ord(phrase[number])-65)
+        print(row_num)
+        print(col_num)
+        number = number + 1
+        encrypted_phrase = encrypted_phrase + vig_matrix[row_num][col_num]
+        
+
+    print(encrypted_phrase)
+
+
+
 
             
         
